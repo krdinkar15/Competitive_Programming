d8f0cf5621fb3e6a667960f071114a2ff6ab0c15
KAMAL RAJ DINKAR <krdinkar15@gmail.com>
2021-04-13 07:18:44 +0530

    "template.cpp added"

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("in.txt","r",stdin);
+        freopen("out.txt","w",stdout);
+    #endif
+    int t=1;
+    cin>>t;  // If there are no test cases then comment it.
+    while(t--)
+    {
+        
+    }
+}
